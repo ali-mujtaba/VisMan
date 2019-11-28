905c7c5cf2773790f702c6b980bc99f2f2f1438e
ali-mujtaba <am.alimujtaba@gmail.com>
2019-11-28 19:11:13 +0530

    Add a route to show details entered by user and another route that updates the check-out time in the database once user enters it

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+var methodOverride = require('method-override');
+const orgAddress = "FRK Hostel, Jamia Millia Islamia"; //update organisation address here
 var mysql = require("mysql");
+const nodemailer = require('nodemailer');
+const Nexmo = require('nexmo');
+
 var pool  = mysql.createPool({
   connectionLimit : 10,
   host            : 'localhost',
@@ -10,7 +15,6 @@ var pool  = mysql.createPool({
   database        : 'visman'
 });
 
-var nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -20,7 +24,6 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-const Nexmo = require('nexmo');
 
 const nexmo = new Nexmo({
   apiKey: '49c7d071',
@@ -31,6 +34,7 @@ const nexmo = new Nexmo({
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
+app.use(methodOverride("_method"));
 
 app.get("/",function(req,res){
 	res.redirect("/new");
@@ -67,13 +71,37 @@ app.post("/new",function(req,res){
 			    // to = "91"+req.body.HPhone; // un-comment to replace hard-coded number with host phone number
 			    
 			    nexmo.message.sendSms(from, to, visitorDetails);
-			    res.redirect("/");
+			    res.redirect("/"+results.insertId);
 			  }
 			});
 		}
 	});
 });
 
+app.get("/:Reference_No",function(req,res){
+	var queryStr = "SELECT * FROM Entries WHERE Reference_No="+req.params.Reference_No+";";
+	pool.query(queryStr,function(error,results,fields){
+		if(error){
+			throw error;
+		}else{
+			res.render("show",{entry:results[0]});
+		}
+	});
+});
+
+app.put("/:Reference_No",function(req,res){
+	var queryStr = "UPDATE Entries SET VCheckOutTime='"+req.body.VCheckOutTime+"' WHERE Reference_No="+req.params.Reference_No+";";
+	
+	var visitDetails = "Visit Details:- \n\nName: "+req.body.VName+"\nPhone: "+req.body.VPhone+"\nCheck-in time: "+req.body.VCheckInTime+"\nCheck-out time: "+req.body.VCheckOutTime+"\nHost Name: "+req.body.HName+"\nAddress Visited: "+orgAddress;
+
+	pool.query(queryStr,function(error,results,fields){
+		if(error){
+			throw error;
+		} else {
+			res.send("Thank you for visiting!");
+		}
+	})
+})
 
 
 app.listen(3000,function(){
