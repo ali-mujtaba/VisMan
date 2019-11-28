698f1477b869baaec75c093d16c5586a6e5c7aba
ali-mujtaba <am.alimujtaba@gmail.com>
2019-11-28 13:22:11 +0530

    Add requirement of host details in the form and change route to form

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
 
 app.get("/",function(req,res){
-	res.render("home");
+	res.redirect("/new");
+});
+
+app.get("/new",function(req,res){
+	res.render("new");
 })
 
 app.listen(3000,function(){
