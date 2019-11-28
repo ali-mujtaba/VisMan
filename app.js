25ea2b2c69354a806a7f9ef1f1776295e9ab2263
ali-mujtaba <am.alimujtaba@gmail.com>
2019-11-28 17:42:32 +0530

    Set up SMS service with Nexmo to text visitor details

--- app.js
+++ app.js
@@ -15,13 +15,18 @@ var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'youremail@gmail.com', // enter you Gmail email here
-    pass: 'yourpassword' // enter password
+    user: 'mujtaba.ali42@gmail.com', // enter you Gmail email here
+    pass: '*ALImujtaba@gmail' // enter password
   }
 });
 
+const Nexmo = require('nexmo');
 
-console.log("DB connected!");
+const nexmo = new Nexmo({
+  apiKey: '49c7d071',
+  apiSecret: 'dVrJ2ThHRqD5L1zC',
+});
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
@@ -36,33 +41,38 @@ app.get("/new",function(req,res){
 });
 
 app.post("/new",function(req,res){
+	
 	var queryStr = "INSERT INTO Entries VALUES (NULL,'"+req.body.VName+"','"+req.body.VEmail+"',"+req.body.VPhone+",'"+req.body.VCheckInTime+"',NULL,'"+req.body.HName+"','"+req.body.HEmail+"',"+req.body.HPhone+");";
-	console.log(queryStr);
+	
 	var visitorDetails = "Visitor Details:- \n\nName: "+req.body.VName+"\nEmail: "+req.body.VEmail+"\nPhone: "+req.body.VPhone;
+	
 	pool.query(queryStr,function(error,results,fields){
 		if(error){
 			throw error;
 		}else{
 			var mailOptions = {
-			  from: 'fromemail@gmail.com', 
-			  to: 'toemail@gmail.com',
+			  from: 'mujtaba.ali42@gmail.com', 
+			  to: 'am.alimujtaba@gmail.com',
 			  subject: 'You have a Visitor',
 			  text: visitorDetails
 			};
-			console.log(mailOptions);
+
 			transporter.sendMail(mailOptions, function(error, info){
 			  if (error) {
 			    console.log(error);
 			  } else {
 			    console.log('Email sent: ' + info.response);
+			    const from = 'VisMan';
+				var to = '917042104749'; // hard coding the host phone number since only this number is whitelisted on nexmo trial account
+			    // to = "91"+req.body.HPhone; // un-comment to replace hard-coded number with host phone number
+			    
+			    nexmo.message.sendSms(from, to, visitorDetails);
+			    res.redirect("/");
 			  }
 			});
 		}
-	})
+	});
 });
-
-
-
 
 
 
